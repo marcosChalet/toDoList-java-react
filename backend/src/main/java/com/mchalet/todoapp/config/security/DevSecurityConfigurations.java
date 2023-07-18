86f3659cbe35588df8fd1291e9df53d2264236d2
marcosChalet <chaletmarcos@gmail.com>
2023-07-18 08:56:36 -0300

    fix: habilitando cors

--- backend/src/main/java/com/mchalet/todoapp/config/security/DevSecurityConfigurations.java
+++ backend/src/main/java/com/mchalet/todoapp/config/security/DevSecurityConfigurations.java
@@ -7,7 +7,7 @@ public class DevSecurityConfigurations implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:5432")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
