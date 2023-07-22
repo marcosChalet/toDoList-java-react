2b917cb798ed85b0a74fb4f90db091fa393596eb
marcosChalet <chaletmarcos@gmail.com>
2023-07-22 15:20:21 -0300

    fix: ajustando tamanho do modal no responsivo

--- frontend/src/components/BaseModal.tsx
+++ frontend/src/components/BaseModal.tsx
@@ -3,7 +3,7 @@ export default function BaseModal({ children }: { children: JSX.Element }) {
     <div className="flex w-full h-full hover:cursor-default">
       <div
         onClick={(e) => e.stopPropagation()}
-        className="m-auto w-[850px] 2xl:w-[1100px] h-4/6 bg-slate-900 rounded-md flex justify-center items-center"
+        className="m-auto w-[850px] 4xl:w-[1100px] h-4/6 bg-slate-900 rounded-md flex justify-center items-center"
       >
         <div className="w-[700px]">{children}</div>
       </div>
